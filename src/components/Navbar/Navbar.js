21524ee05f12e702f2dcc3984be5ab4ba390e125
asn-athira <athira@rightsolution.ae>
2021-02-24 16:40:47 +0530

    first commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,17 @@ const Navbar = (props) => {
 
 return (
 			
-			<div className="navbar">
-				
+			<div className="navbar">				
 			  <ul>
 			    <li><img src="/logo1.png" className="logo" alt=""/></li>
-			    <li><a href="">About</a></li>
-			    <li><a href="">Services</a></li>
-			    <li><a href="">About</a></li>
-			    <li><a href="">Services</a></li>
-			    <li><a href="">Offers</a></li>
-			    <li><a href="">Pricing</a></li>
-			    <li><a href="">FAQ</a></li>
-			    <li><a href="">Locations</a></li>	
+			    <li>About</li>
+			    <li>Services</li>
+			    <li>About</li>
+			    <li>Services</li>
+			    <li>Offers</li>
+			    <li>Pricing</li>
+			    <li>FAQ</li>
+			    <li>Locations</li>	
 			    <li><button className="navbar-button">&nbsp;<i className="fa fa-whatsapp"></i>
               whats up</button></li>	
           <li ><p className="phone"> Call us for 30% off on your first order!<br /><p className="phone-text">+971 521357033</p><br />We are Avaliable 24/7
